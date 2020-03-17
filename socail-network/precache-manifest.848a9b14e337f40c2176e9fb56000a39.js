e2cd3c75d83ab2b2f2864e4c2977ec9f98d07e88
Valeriy Yeroshenko <valeriy.yeroshenko@gmail.com>
2020-03-17 15:14:29 +0200

    Add social network

---
+++ socail-network/precache-manifest.848a9b14e337f40c2176e9fb56000a39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2470a32ff344d6eb3534a4138705017e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dee0efde7355f3618587",
+    "url": "/static/css/main.3f955174.chunk.css"
+  },
+  {
+    "revision": "0abc05756a84fae5894b",
+    "url": "/static/js/2.ad984531.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/static/js/2.ad984531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee0efde7355f3618587",
+    "url": "/static/js/main.22888f58.chunk.js"
+  },
+  {
+    "revision": "4d5c7dc3fd71e36f1842",
+    "url": "/static/js/runtime-main.ae6a0aee.js"
+  },
+  {
+    "revision": "e1c0855b0babf7c6bf7a15749e2fe193",
+    "url": "/static/media/avatar-substrate.e1c0855b.svg"
+  },
+  {
+    "revision": "230b83854503c44a425f229105982b9d",
+    "url": "/static/media/logo.230b8385.svg"
+  },
+  {
+    "revision": "5bbc97f832477e3876d68a0e57185a19",
+    "url": "/static/media/polygon.5bbc97f8.svg"
+  },
+  {
+    "revision": "d9362f6fe510a413998616579798c221",
+    "url": "/static/media/user-wallpaper.d9362f6f.jpg"
+  },
+  {
+    "revision": "7cc7a630624d20f7797cb4c8e93c09c1",
+    "url": "/static/media/user.7cc7a630.png"
+  }
+]);
